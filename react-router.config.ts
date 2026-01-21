d1f2e0bb7cfb826b4bb26e25b72352f51985b24d
rezaarubusman <fahreza.arubusman@gmail.comgit config --global user.name rezaarubusmangit config --global user.email fahreza.arubusman@gmail.com>
2026-01-21 17:59:44 +0700

    update react-router.config.ts

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,12 +1,7 @@
 import type { Config } from "@react-router/dev/config";
 
 export default {
-  // MATIKAN SSR → jadi SPA
-  ssr: false,
-
-  // default sudah cukup, tapi kita tulis biar jelas
-  appDirectory: "app",
-
-  // output build tetap default
-  // hasilnya: build/client/*
+  // Config options...
+  // Server-side render by default, to enable SPA mode set this to `false`
+  ssr: true,
 } satisfies Config;
