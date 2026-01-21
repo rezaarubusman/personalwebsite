761a7a3e221682d9ecc6302d33e1a07b4f2f4809
rezaarubusman <fahreza.arubusman@gmail.comgit config --global user.name rezaarubusmangit config --global user.email fahreza.arubusman@gmail.com>
2026-01-21 17:16:13 +0700

    perbaikan react-router.config

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,5 +1,12 @@
 import type { Config } from "@react-router/dev/config";
 
 export default {
-  buildDirectory: ".vercel/output"
+  // MATIKAN SSR → jadi SPA
+  ssr: false,
+
+  // default sudah cukup, tapi kita tulis biar jelas
+  appDirectory: "app",
+
+  // output build tetap default
+  // hasilnya: build/client/*
 } satisfies Config;
